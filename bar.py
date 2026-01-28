aadd17975c657c8733923a7ef3c9c37a98cd6ea8
Raj <rajbhai6154@gmail.com>
2026-01-28 13:30:32 +0530

    Cleanup: Remove unrelated project files from repo

--- bar.py
+++
@@ -1,6 +0,0 @@
-import time
-from tqdm import tqdm
-
-
-for i in tqdm(range(50), desc="Processing"):
-    time.sleep(0.1)
--- query.sql
+++
@@ -1,5 +0,0 @@
--- Add a new employee named 'Gemini'
-INSERT INTO employees (name, salary) VALUES ('Gemini', 0);
-
--- Show me the full list of employees
-SELECT * FROM employees;
